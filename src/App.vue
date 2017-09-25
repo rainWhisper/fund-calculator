c55985a983964226cbdaad6ad6e2d05a950e9b1c
zhshy <zhshy123@foxmail.com>
2017-09-25 23:49:12 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
